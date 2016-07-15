cdcc62ad4662c0c49a556aa9a83194c94d702c08
roy <rcdosado@gmail.com>
2016-07-15 19:43:32 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# django_blog
+a blog written in python django. users can comment, can send email. for learning purposes only. 
---
+++ blog/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
+from django.core.mail import send_mail
+from .models import Post, Comment
+from django.views.generic import ListView 
+from .forms import EmailPostForm, CommentForm
+
+
+class PostListView(ListView):
+	queryset = Post.published.all()
+	context_object_name = 'posts'
+	paginate_by = 3 
+	template_name = 'blog/post/list.html'
+
+def post_share(request, post_id):
+	# import pdb; pdb.set_trace()
+	post = get_object_or_404(Post, id=post_id, status='published')
+	sent = False
+
+	if request.method == 'POST':
+		form = EmailPostForm(request.POST)
+		if form.is_valid():
+			cd = form.cleaned_data
+			post_url = request.build_absolute_uri(
+				post.get_absolute_url())
+			subject = '{} ({}) recommends you reading "{}"'.format(cd['name'],cd['email'], post.title)
+			message='Read "{}" at {}\n\n{}\'s comments:{}'.format(post.title, post_url, cd['name'],cd['comments'])
+			send_mail(subject, message, 'admin@mysite.com',[cd['to']])
+			sent=True
+	else: 
+		form = EmailPostForm() 
+
+	return render(request,'blog/post/share.html', {'post':post,
+		'form':form,
+		'sent':sent})
+
+def post_list(request):
+	object_list = Post.published.all()
+	paginator = Paginator(object_list, 3)
+	page = request.GET.get('page')
+
+	try:
+		posts = paginator.page(page)
+	except PageNotAnInteger:
+		posts = paginator.page(1)
+	except EmptyPage:
+		posts = paginator.page(paginator.num_pages)
+	return render(request,
+		'blog/post/list.html',
+		{'page':page,
+		'posts':posts})
+
+def post_detail(request, year, month, day, post):
+	post = get_object_or_404(Post, slug=post,
+		status='published',
+		publish__year=year,
+		publish__month=month,
+		publish__day=day)
+	# List of active comments for this post 
+	comments = post.comments.filter(active=True)
+	# import pdb; pdb.set_trace()
+	if request.method == 'POST':
+		comment_form = CommentForm(data = request.POST)
+		if comment_form.is_valid():
+			#Create Comment Object but don't save to database yet 
+			new_comment = comment_form.save(commit=False)
+			#assign the current post to the comment 
+			new_comment.post = post 
+			#save the comment to the database 
+			new_comment.save()
+			comment_form = CommentForm()
+	else:
+		comment_form = CommentForm()
+	return render(request, 'blog/post/detail.html',
+		{'post':post,
+		'comments':comments,
+		'comment_form':comment_form})
+
