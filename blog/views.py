f957a6936b35c2572d1317007130c5a020e49dd6
roy <rcdosado@gmail.com>
2016-07-16 23:22:28 +0800

    Add a way to print all post that have a specific tag

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.core.mail import send_mail
 from .models import Post, Comment
 from django.views.generic import ListView 
 from .forms import EmailPostForm, CommentForm
-
+from taggit.models import Tag 
 
 class PostListView(ListView):
 	queryset = Post.published.all()
@@ -34,8 +34,14 @@ def post_share(request, post_id):
 		'form':form,
 		'sent':sent})
 
-def post_list(request):
+def post_list(request, tag_slug=None):
 	object_list = Post.published.all()
+	tag = None 
+
+	if tag_slug:
+		tag = get_object_or_404(Tag, slug=tag_slug)
+		object_list = object_list.filter(tags__in=[tag])
+
 	paginator = Paginator(object_list, 3)
 	page = request.GET.get('page')
 
@@ -48,7 +54,8 @@ def post_list(request):
 	return render(request,
 		'blog/post/list.html',
 		{'page':page,
-		'posts':posts})
+		'posts':posts,
+		'tag':tag})
 
 def post_detail(request, year, month, day, post):
 	post = get_object_or_404(Post, slug=post,
