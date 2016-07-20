faa3aa435a0ead4cb3043d7c7f1125fc4e599118
roy <rcdosado@gmail.com>
2016-07-20 21:10:59 +0800

    add route to default $ page to admin

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include,url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
+	url(r'^$',RedirectView.as_view(url='/admin/')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls',namespace='blog',app_name='blog')),
 ]
