6ba3ceebfbfcc3e7c6e6ae960a653892b01ec363
roy <rcdosado@gmail.com>
2016-07-26 22:28:46 +0800

    Add sitemap to this django site

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1 
 
 # Application definition
 
@@ -42,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'blog',
     'taggit',
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,17 @@ Including another URLconf
 from django.conf.urls import include,url
 from django.contrib import admin
 from django.views.generic import RedirectView
+from django.contrib.sitemaps.views import sitemap 
+from blog.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts':PostSitemap,
+}
 
 urlpatterns = [
 	url(r'^$',RedirectView.as_view(url='/blog/')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls',namespace='blog',app_name='blog')),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps':sitemaps}, 
+        name='django.contrib.sitemaps.views.sitemap'), 
 ]
