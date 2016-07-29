e27855c283c3d11ac64349ee76ddbf02155c7e0c
roy <rcdosado@gmail.com>
2016-07-29 22:54:23 +0800

    Update RSS Feeds

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
 from . import views
+from .feeds import LatestPostsFeed
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'), 
@@ -9,4 +10,6 @@ urlpatterns = [
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\
     	r'(?P<post>[-\w]+)/$',views.post_detail, name='post_detail'),
     url(r'^(?P<post_id>\d+)/share/$', views.post_share,name='post_share'),
+
+    url(r'^feed/$', LatestPostsFeed(), name='post_feed'),
 ]
